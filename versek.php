33e07f91c938d01eee214faccd1ff4969109a09b
Balla Csanád <balla.csanad@prioris.hu>
2019-04-30 21:35:20 +0200

    Védésre javítások

--- versek.php
+++ versek.php
@@ -9,19 +9,28 @@
         $result = $conn->query($sql);
         $resultCeck = mysqli_num_rows($result);
         if ($resultCeck > 0) {
+            $i = 0;
             while($row = mysqli_fetch_assoc($result))
             {   
                 echo '<div class="poszt">';
                 if ($row['cim'] != NULL) {
                     echo   '<h2>' . $row['cim'] . '</h2>';
                 }
-
-                    $utvonal = $row['utvonal'];
-                    echo '<img src=' . $utvonal . '>';
-                
-                if ($row['szoveg'] != NULL) {
-                    echo   '<p>' . $row['szoveg'] . '</p>';
+                    ?>
+                    <div class="row">
+                        <div class="col-12 col-md-6 <?php if($i%2!=0){ echo 'order-2';} ?>">
+                        <?php $utvonal = $row['utvonal'];
+                            echo '<img src=' . $utvonal . '>'; ?>
+                        </div>
+                    
+                   
+                <?php
+                if ($row['szoveg'] != NULL) { ?>
+                    <div class="col-12 col-md-6" <?php if($i%2!=0){ echo 'style="text-align:right"';} ?>>
+                    <?php echo   '<p>' . $row['szoveg'] . '</p>';
+                    echo '</div>';
                 }
+                    echo '</div>';
                 if ($row['video'] != NULL) {
                     $video = $row['video'];
                     echo '<div class="embed-responsive embed-responsive-16by9">
@@ -31,14 +40,15 @@
 
                 if (isset($_SESSION['username'])){
                     echo '<div class ="row"><div class="col-6">
-                    <a href="poszt-szerkesztes.php?id=' .$row['Post_ID'] .'">Szerkesztés</a>';
-                    echo '<a href="poszt-torles.php?id=' .$row['Post_ID'] .'">Törlés</a></div>';
+                    <a href="poszt-szerkesztes.php?id=' .$row['Post_ID'] .'"><i class="fas fa-edit"></i> Szerkesztés</a>';
+                    echo '<a href="#" onclick="confirmTorles(\'poszt-torles.php?id=' .$row['Post_ID'] .'\')"><i class="fas fa-trash-alt"></i> Törlés</a></div>';
                     echo '<div class="col-6"><p class="date">' . $row['datum'] . '</p></div></div>';
                 }else {
                     echo '<p class="date">' . $row['datum'] . '</p>';
                 }
                 
                 echo '</div>';
+                $i++;
             }
         }
     ?>
